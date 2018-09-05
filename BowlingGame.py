50eec1c34898531eb8e64fc1a84f353fa1321475
Purnima Ghosh <purnima@vt.edu>
2018-09-05 12:14:41 -0400

    First commit

---
+++ BowlingGame.py
@@ -0,0 +1,7 @@
+class Game():
+    
+    def rolls(pins):
+        #called each time the player rolls a ball
+
+    def score():
+        #called at the end of the game and returns total score
---
+++ BowlingGameTest.py
@@ -0,0 +1,10 @@
+from BowlingGame import Game
+import unittest
+
+class TestBowlingGame(unittest.TestCase):
+    game = Game()
+
+    def setUp(self):
+        self.game = Game()
+
+    def testGutterGame()

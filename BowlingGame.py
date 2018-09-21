2e8680aac036e3d6de05b49826c693d3d626edec
Purnima Ghosh <purnima.ghosh@pghosh.2ldxhoqz4w0efi5ajh3u0k0m2g.bx.internal.cloudapp.net>
2018-09-21 17:02:09 +0000

    created a few more tests

--- BowlingGame.py
+++ BowlingGame.py
@@ -2,10 +2,16 @@ class Game():
     
     
     score = 0
+    rolls = [0] * 21
+    currRoll = 0
 
     def roll(self, pins):
         self.score += pins
+        self.rolls[++self.currRoll] = pins
 
     #called at the end of the game and returns total score
     def getScore(self):
+        self.score = 0
+        for i in self.rolls:
+            self.score += self.rolls[i]
         return self.score
--- BowlingGameTest.py
+++ BowlingGameTest.py
@@ -6,6 +6,10 @@ class BowlingGameTest(unittest.TestCase):
     def setUp(self):
         self.game = Game()
 
+    def rollMany(self, n, pins):
+        for i in range(n):
+            self.game.roll(pins)
+
     def test_guttergame(self):
         for i in range(20):
             self.game.roll(0)
@@ -17,6 +21,17 @@ class BowlingGameTest(unittest.TestCase):
             self.game.roll(1)
         
         assert self.game.getScore() == 20
+
+    def test_roll_many(self):
+        self.rollMany(20, 1)
+        assert self.game.getScore() == 20
+    
+    def test_one_spare(self):
+        self.game.roll(5)
+        self.game.roll(5)
+        self.game.roll(3)
+        self.rollMany(17,0)
+        assert 13 == self.game.getScore()
 
 if __name__ == '__main__':
     unittest.main()

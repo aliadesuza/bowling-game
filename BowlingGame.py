b8817395019b07b8aa4867bb31976bb51611256d
Purnima Ghosh <purnima.ghosh@pghosh.2ldxhoqz4w0efi5ajh3u0k0m2g.bx.internal.cloudapp.net>
2018-09-24 17:32:42 +0000

    everything is broken

--- BowlingGame.py
+++ BowlingGame.py
@@ -6,12 +6,14 @@ class Game():
     currRoll = 0
 
     def roll(self, pins):
-        self.score += pins
-        self.rolls[++self.currRoll] = pins
+        self.rolls[self.currRoll] = pins
+        self.currRoll += 1
 
     #called at the end of the game and returns total score
     def getScore(self):
         self.score = 0
         for i in self.rolls:
             self.score += self.rolls[i]
+            if self.rolls[i] + self.rolls[i+1] == 10:
+                self.score += self.rolls[i+2]        
         return self.score
--- BowlingGameTest.py
+++ BowlingGameTest.py
@@ -5,6 +5,7 @@ class BowlingGameTest(unittest.TestCase):
 
     def setUp(self):
         self.game = Game()
+        self.longMessage = True
 
     def rollMany(self, n, pins):
         for i in range(n):
@@ -17,21 +18,24 @@ class BowlingGameTest(unittest.TestCase):
         assert self.game.getScore() == 0
 
     def test_all_ones(self):
-        for i in range(20):
-            self.game.roll(1)
-        
-        assert self.game.getScore() == 20
+        # for i in range(20):
+        #     self.game.roll(1)
+        self.rollMany(19, 1)
+        expectedScore = 20
+        self.assertEqual(expectedScore, self.game.getScore(), 'incorrect score')
 
-    def test_roll_many(self):
-        self.rollMany(20, 1)
-        assert self.game.getScore() == 20
+    # def test_roll_many(self):
+    #     self.rollMany(19, 1)
+    #     expectedScore = 20
+    #     self.assertEqual(expectedScore, self.game.getScore(), 'incorrect score')
     
     def test_one_spare(self):
         self.game.roll(5)
         self.game.roll(5)
         self.game.roll(3)
-        self.rollMany(17,0)
-        assert 13 == self.game.getScore()
+        # self.rollMany(17,0)
+        expectedScore = 16
+        self.assertEqual(expectedScore, self.game.getScore(), 'incorrect score')
 
 if __name__ == '__main__':
     unittest.main()
